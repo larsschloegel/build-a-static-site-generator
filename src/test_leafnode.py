ecc83c3dfc168f426872caf7e1ba1afaeda3a4a2
lschloegel <lars.schloegel@gmail.com>
2026-06-05 20:28:12 +0200

    CH2 L4: LeafNode

---
+++ src/test_leafnode.py
@@ -0,0 +1,57 @@
+import unittest
+from leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(
+            node.to_html(),
+            "<p>Hello, world!</p>"
+        )
+
+    def test_leaf_to_html_anchor(self):
+        node = LeafNode(
+            "a",
+            "Click me!",
+            {"href": "https://www.google.com"}
+        )
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>'
+        )
+
+    def test_leaf_to_html_raw_text(self):
+        node = LeafNode(None, "Just text")
+        self.assertEqual(
+            node.to_html(),
+            "Just text"
+        )
+
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_leaf_with_multiple_props(self):
+        node = LeafNode(
+            "a",
+            "Link",
+            {
+                "href": "https://example.com",
+                "target": "_blank"
+            }
+        )
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://example.com" target="_blank">Link</a>'
+        )
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+
+if __name__ == "__main__":
+    unittest.main()
