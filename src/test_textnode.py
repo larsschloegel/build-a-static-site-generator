9b4a66387d4840bcf289393cf6d92014b31b436f
lschloegel <lars.schloegel@gmail.com>
2026-06-07 16:19:48 +0200

    CH2 L6: TextNode to HTMLNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -17,6 +17,62 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("without url", TextType.BOLD)
         node2 = TextNode("with url", TextType.ITALIC, "https:test.de")
         self.assertNotEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", TextType.TEXT,
+                        "https://www.boot.dev")
+        node2 = TextNode("This is a text node", TextType.TEXT,
+                         "https://www.boot.dev")
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.TEXT,
+                        "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
+
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE,
+                        "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
+
+    def test_bold(self):
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
+
+    """ def split_nodes_delimiter(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = self.split_nodes_delimiter([node], "`", TextType.CODE)
+
+        result = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, result) """
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from leafnode import LeafNode
 
 
 class TextType(Enum):
@@ -10,8 +11,7 @@ class TextType(Enum):
     IMAGE = "image"
 
 
-class TextNode():
-
+class TextNode:
     def __init__(self, text: str, text_type: TextType, url: str | None = None) -> None:
         self.text = text
         self.text_type = text_type
@@ -21,4 +21,34 @@ class TextNode():
         return self.text == other.text and self.text_type == other.text_type and self.url == other.url
 
     def __repr__(self) -> str:
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node: "TextNode") -> LeafNode:
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+
+    elif text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {
+            "src": text_node.url,
+            "alt": text_node.text
+        })
+    else:
+        raise ValueError(f"invalid text type: {text_node.text_type}")
+
+
+""" 
+    def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+        new_nodes = []
+        splited = old_nodes.split(delimiter) """
