ffbc277547cee7b2279ef71988da2a4b40aea558
lschloegel <lars.schloegel@gmail.com>
2026-06-29 11:59:36 +0200

    CH5 L5: deploy to github pages

--- src/main.py
+++ src/main.py
@@ -8,11 +8,13 @@ dir_path_static = "./static"
 dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
 def main():
-    basepath = sys.argv[0]
-    if not basepath:
-        basepath = "/"
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
